d288bdf3379554c44b3c6d6de48c7fd81758264d
soryrawyer <sory.rawyer@gmail.com>
2018-05-05 14:28:34 -0400

    update serial to have latest version of portreader

--- examples/serial.rs
+++ examples/serial.rs
@@ -9,34 +9,13 @@ use std::thread;
 
 
 fn main() {
-    // if let Ok(mut port) = serialport::open("/dev/cu.usbmodem1411") {
-    //     let mut serial_buf: Vec<u8> = vec![0; 5];
-    //     println!("receiving data?");
-    //     loop {
-    //         match port.read(serial_buf.as_mut_slice()) {
-    //             Ok(t) => {
-    //                 let sensor_val: String = String::from_utf8(serial_buf[..t].to_vec()).unwrap();
-    //                 println!("{:?}", sensor_val);
-    //             },
-    //             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-    //             Err(e) => println!("got this error: {:?}", e),
-    //         }
-    //     }
-    // } else {
-    //     println!("couldn't open the port!");
-    // }
-
-    // let mut pr = PortReader::new("/dev/cu.usbmodem1411");
+    let mut pr = PortReader::new("/dev/cu.usbmodem1411");
     let (tx, rx): (SyncSender<i32>, Receiver<i32>) = mpsc::sync_channel(1);
     thread::spawn(move || {
         for i in 0..10000 {
-            // let mut now = Instant::now();
             if let Ok(_) = tx.try_send(i) {
                 continue;
-            } else {
-                continue;
             }
-            // times.push(now.elapsed().subsec_nanos());
         }
     });
 
@@ -48,12 +27,7 @@ fn main() {
             println!("byeeeee!");
             break;
         }
-        // println!("{:?}", now.elapsed());
     }
-
-    // let total_nanos: u32 = times.iter().sum();
-
-    // println!("{:?}", total_nanos as f32 / times.len() as f32);
 }
 
 struct PortReader {
@@ -80,35 +54,18 @@ impl PortReader {
         loop {
             match self.port.read(serial_buf.as_mut_slice()) {
                 Ok(t) => {
-                    // println!("{:?}", serial_buf);
                     for i in 0..t {
-                        // if we're at a newline:
-                        //   append buffer to self.unread_stuff, then clear self.unread_stuff
                         if serial_buf[i] == 10 {
-                            println!("{:?}", value);
                             self.unread_stuff = serial_buf[(i+1)..t].to_vec();
-                            return 0
-                            // String::from_utf8(value).unwrap().parse().unwrap()
-
-                            // match value.len() {
-                            //     0 => 0,
-                            //     1 => (&value[..]).read_i8().unwrap() as i32,
-                            //     2 => (&value[..]).read_i16::<LittleEndian>().unwrap() as i32,
-                            //     3 => (&value[..]).read_i24::<LittleEndian>().unwrap() as i32,
-                            //     4 => (&value[..]).read_i32::<LittleEndian>().unwrap(),
-                            //     5 => (&value[..]).read_i32::<LittleEndian>().unwrap(),
-                            //     6 => (&value[..]).read_i32::<LittleEndian>().unwrap(),
-                            //     _ => {
-                            //         println!("{:?}", value.len());
-                            //         panic!("nooooo!")
-                            //     },
-                            // }
-                            // return (&value[..]).read_i32::<BigEndian>().unwrap();
+                            if let Ok(val) = String::from_utf8(value).unwrap().parse() {
+                                return val
+                            } else {
+                                return 0
+                            }
                         } else {
                             value.extend(vec!(serial_buf[i]));
                         }
                     }
-                    // let sensor_val = String::from_utf8(buf).unwrap();
                 },
                 Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
                 Err(e) => println!("got this error: {:?}", e),
@@ -117,3 +74,4 @@ impl PortReader {
     }
 
 }
+
